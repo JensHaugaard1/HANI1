d43d33034cbca09bf4c1d657f87e5889673b5cfc
JensHaugaard1 <136031062+JensHaugaard1@users.noreply.github.com>
2024-06-12 23:29:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var cursor = document.getElementById("cursor");
+document.body.addEventListener("mousemove", function(e) {
+  cursor.style.left = e.clientX + "px",
+    cursor.style.top = e.clientY + "px";
+});
+$(window).scroll(function(){
+    if ($(window).scrollTop() == $(document).height()-$(window).height()){
+       $.ajax({
+          url: "index.html",
+          success: function (data) { $('body').append(data); },
+          dataType: 'html'
+       });
+    }
+});
