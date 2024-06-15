6d819e7cf175ec83dcb540da6bcafabd824dccf7
JensHaugaard1 <136031062+JensHaugaard1@users.noreply.github.com>
2024-06-15 11:06:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ $(window).scroll(function(){
        });
     }
 });
+
+$(function(){
+   $("a").click(function(evt){
+       var link = $(this).attr("href");
+       setTimeout(function() {
+           window.location.href = link;
+       }, 500);
+   });
+});
